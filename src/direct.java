fa57bf044c3395deb0fdb7cb70ecde69c49d88fe
Davi Souza Carmo <0001159198@senaimgaluno.com.br>
2025-12-01 10:55:28 -0300

    primeiro commit

---
+++ src/direct.java
@@ -0,0 +1,10 @@
+import java.time.LocalDate;
+
+public class direct {
+    private int id;
+    private int remetenteId;
+    private int destinatarioId;
+    private String mensagem;
+    private LocalDate dataEnvio;
+
+}
